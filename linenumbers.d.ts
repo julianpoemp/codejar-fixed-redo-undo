26eb4f3859d566ffd7c80456d431678f0e696194
Julian Pömp <jp@sp-computing.de>
2023-02-12 11:47:55 +0100

    changed gitignore

---
+++ linenumbers.d.ts
@@ -0,0 +1,9 @@
+type Options = {
+    class: string;
+    wrapClass: string;
+    width: string;
+    backgroundColor: string;
+    color: string;
+};
+export declare function withLineNumbers(highlight: (e: HTMLElement) => void, options?: Partial<Options>): (editor: HTMLElement) => void;
+export {};
